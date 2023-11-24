4b213c0e5bbe2d7a079d7bde11ba4cb4e42127c1
samsonmobisa <samsonmobisa62@gmail.com>
2023-11-24 14:38:58 +0300

    Elixir docs

--- lib/struct.ex
+++ lib/struct.ex
@@ -1,14 +1,14 @@
 defmodule Struct do
   # 1.Defining a Struct
-  #Struct is defined inside the context of a module.
-  #Struct can be used to define a custom data type that hold diffrent fields.
+  # Struct is defined inside the context of a module.
+  # Struct can be used to define a custom data type that hold diffrent fields.
   # %Struct{}
   # Using a pipe operator
             #|> example %Struct{}|> Map.get(:name)
             #|> example %Struct{}|> Map.get(:age)
             #|> example %Struct{}|> Map.get(:gender)
-
+  #
   defstruct [name: "Sam", age: 40, gender: "male"]
-  
+
 
 end
